ba80ee45e14ec56d644b50ba064563395f80acb9
emon khan <bdemon00@gmail.com>
2022-05-21 23:53:06 +0600

    System admin user table seeder added

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'System Admin',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
